b7f9bedd609e0f630e46593a84abb870e0eb1f7d
Oscar Tisnado <68830577+otisnado@users.noreply.github.com>
2022-06-01 12:35:54 -0600

    feat: setup main.go

--- main.go
+++ main.go
@@ -1,5 +1,16 @@
 package main
 
-func main() {
+import (
+	"log"
 
+	"github.com/otisnado/sn-api/db"
+	"github.com/otisnado/sn-api/handlers"
+)
+
+func main() {
+	if db.CheckConnection() == 0 {
+		log.Fatal("Sin conexion a la base de datos")
+		return
+	}
+	handlers.Handlers()
 }
