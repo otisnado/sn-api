6617f93dfa317375f654473c87c106620cc64876
Oscar Tisnado <68830577+otisnado@users.noreply.github.com>
2022-06-05 11:25:40 -0600

    feat: adding check if an user exists

---
+++ db/checkUserExists.go
@@ -0,0 +1,28 @@
+package db
+
+import (
+	"context"
+	"time"
+
+	"github.com/otisnado/sn-api/models"
+	"go.mongodb.org/mongo-driver/bson"
+)
+
+func CheckUserExists(email string) (models.User, bool, string) {
+	ctx, cancel := context.WithTimeout(context.Background(), 15*time.Second)
+	defer cancel()
+
+	db := MongoCN.Database("twittor")
+	col := db.Collection("users")
+
+	condition := bson.M{"email": email}
+
+	var result models.User
+
+	err := col.FindOne(ctx, condition).Decode(&result)
+	ID := result.ID.Hex()
+	if err != nil {
+		return result, false, ID
+	}
+	return result, true, ID
+}
